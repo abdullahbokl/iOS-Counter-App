b0a4d7e34f4ed0af56c091f3e7651ba52a68da2e
Eng.Bokl <abdullah.bokl@gmail.com>
2026-02-21 23:15:37 +0200

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Abdullah Elbokl on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
